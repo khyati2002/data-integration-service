ecfa63175446284aa87a054112646b2fa0aa8b87
Gaurav Kumar Gupta <gaurav.kumar@salescode.ai>
2024-12-10 12:34:03 +0530

    optimization

--- src/main/java/com/salescode/channelkart/utils/EntityUtils.java
+++ src/main/java/com/salescode/channelkart/utils/EntityUtils.java
@@ -23,6 +23,7 @@ import org.springframework.beans.BeanWrapperImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
 import javax.persistence.NoResultException;
 import java.io.*;
 import java.lang.reflect.Field;
@@ -42,6 +43,7 @@ public final class EntityUtils {
     private static EntityUtils instance;
     private final MetadataRegistry metadataRegistry;
     private final DSLContext dslContext;
+    Set<Class<? extends CommonDataModel>> subClasses = ReflectionUtils.findSubClasses(CommonDataModel.class);
 
 
     @Autowired
@@ -168,7 +170,6 @@ public final class EntityUtils {
         if (entityClassMap.containsKey(entityName)) {
             return entityClassMap.get(entityName);
         }
-        Set<Class<? extends CommonDataModel>> subClasses = ReflectionUtils.findSubClasses(CommonDataModel.class);
         for (var entity : subClasses) {
             if (entityName.equalsIgnoreCase(entity.getSimpleName())) {
                 entityClassMap.put(entityName, entity);
