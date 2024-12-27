610205ac2fb9b2019c5e42d4e37f0b9deca8ec4f
khyati12arora <khyati.arora@salescode.ai>
2024-12-27 16:19:38 +0530

    updated AppCacheManager

--- src/main/java/com/salescode/channelkart/cache/AppCacheManager.java
+++ src/main/java/com/salescode/channelkart/cache/AppCacheManager.java
@@ -59,9 +59,9 @@ public class AppCacheManager {
     }
 
     public void put(String lob, String key, Object value) {
-        String cacheName = lob != null ? lob : COMMON_CACHE_NAME;
-        Cache<String, Object> cache = ensureCache(cacheName);
-        cache.put(key, value);
+//        String cacheName = lob != null ? lob : COMMON_CACHE_NAME;
+//        Cache<String, Object> cache = ensureCache(cacheName);
+//        cache.put(key, value);
     }
 
     public <V> V withCache(String cacheDomain, String key, Function<String, V> function) {
