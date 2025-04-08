0c6baa14a449514285279a2135bb4b5bd443455c
krishna-salescode <141234997+krishna-salescode@users.noreply.github.com>
2025-04-08 17:33:45 +0530

    added bepensa transformer

---
+++ src/main/java/com/applicate/Bepensa/DeliveryPJPtransformer.java
@@ -0,0 +1,100 @@
+package com.applicate.bepensa.transformer;
+
+import com.applicate.services.channelkart.exceptions.TransformationException;
+import com.applicate.services.channelkart.services.CustomerAccountsService;
+import com.applicate.services.channelkart.services.SpringContext;
+import com.applicate.services.channelkart.transformers.AbstractTransformer;
+import com.applicate.services.channelkart.utils.DateUtils;
+import com.applicate.services.channelkart.utils.JSONUtils;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+import java.text.SimpleDateFormat;
+import java.time.DayOfWeek;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.TemporalAdjusters;
+import java.util.*;
+
+public class DeliveryPJPtransformer extends AbstractTransformer<Map<String, Object>, Map<String, Object>> {
+
+    private final CustomerAccountsService accountsService = SpringContext.getBean(CustomerAccountsService.class);
+
+    @Override
+    public List<Map<String, Object>> transform(Map<String, Object> inputMap) {
+        List<Map<String, Object>> responseList = new ArrayList<>();
+
+        LocalDateTime currentDate = LocalDateTime.now(); // get current date
+        int dayOfWeek = Integer.parseInt(inputMap.get("day").toString());
+        LocalDateTime firstDesiredDayOfCurrentMonth = currentDate.with(TemporalAdjusters.firstDayOfMonth()).with(TemporalAdjusters.nextOrSame(getDayOfWeek(dayOfWeek)));
+
+        // Add dates for the current month
+        responseList.addAll(getAllDesiredDaysInMonth(inputMap, firstDesiredDayOfCurrentMonth));
+
+        // Add dates for the next month only if visit_type is 1
+        if ("1".equals(inputMap.get("visit_type").toString())) {
+            LocalDateTime firstDesiredDayOfNextMonth = currentDate.plusMonths(1).with(TemporalAdjusters.firstDayOfMonth()).with(TemporalAdjusters.nextOrSame(getDayOfWeek(dayOfWeek)));
+            responseList.addAll(getAllDesiredDaysInMonth(inputMap, firstDesiredDayOfNextMonth));
+        }
+
+        if (responseList.isEmpty()) {
+            throw new TransformationException("Day not available for the current month");
+        }
+        return responseList;
+    }
+
+    private DayOfWeek getDayOfWeek(int day) {
+        switch (day) {
+            case 1:
+                return DayOfWeek.SUNDAY;
+            case 2:
+                return DayOfWeek.MONDAY;
+            case 3:
+                return DayOfWeek.TUESDAY;
+            case 4:
+                return DayOfWeek.WEDNESDAY;
+            case 5:
+                return DayOfWeek.THURSDAY;
+            case 6:
+                return DayOfWeek.FRIDAY;
+            case 7:
+                return DayOfWeek.SATURDAY;
+            default:
+                throw new TransformationException("Invalid day value: " + day);
+        }
+    }
+
+    private List<Map<String, Object>> getAllDesiredDaysInMonth(Map<String, Object> inputMap, LocalDateTime firstDesiredDayOfMonth) {
+        List<Map<String, Object>> responseList = new ArrayList<>();
+        LocalDateTime currentDate = firstDesiredDayOfMonth;
+
+        while (currentDate.getMonth() == firstDesiredDayOfMonth.getMonth()) {
+            Map<String, Object> responseMap = createResponseMap(inputMap, currentDate);
+            responseList.add(responseMap);
+            currentDate = currentDate.plusWeeks(1);
+        }
+
+        return responseList;
+    }
+
+    private Map<String, Object> createResponseMap(Map<String, Object> inputMap, LocalDateTime date) {
+        Map<String, Object> responseMap = new HashMap<>();
+        responseMap.put("beat", inputMap.get("idruta") != null ? inputMap.get("idruta").toString() : "");
+        responseMap.put("loginId", inputMap.get("codemp") != null ? inputMap.get("codemp").toString() : "");
+        responseMap.put("outletCode", inputMap.get("Customer_Code") != null ? inputMap.get("Customer_Code").toString() : "");
+        responseMap.put("type", inputMap.get("visit_type") != null ? inputMap.get("visit_type").toString() : "");
+        responseMap.put("supplierId", inputMap.get("branch_code") != null ? inputMap.get("branch_code").toString() : "");
+        responseMap.put("month", date.getMonth().name());
+        responseMap.put("year", String.valueOf(date.getYear()));
+        responseMap.put("activeStatus", inputMap.get("activo") != null ? addStatus(inputMap.get("activo").toString()) : "");
+        responseMap.put("pjpPlan", inputMap.get("day") != null ? inputMap.get("day").toString() : "");
+        date = date.withHour(0).withMinute(0).withSecond(0).withNano(0);
+        responseMap.put("pjpDate", Date.from(date.atZone(ZoneId.systemDefault()).toInstant()));
+        return responseMap;
+    }
+
+    public static String addStatus(String status) {
+        return status.equals("true") ? "active" : "inactive";
+    }
+}
