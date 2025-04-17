8070c549f48664e6627ecb3d34bbeb9ac251ec17
Gaurav Kumar Gupta <gaurav.kumar@salescode.ai>
2025-04-17 12:10:36 +0530

    add insights project

---
+++ insights/src/test/java/com/salescode/dis/insights/InsightsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salescode.dis.insights;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsightsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
