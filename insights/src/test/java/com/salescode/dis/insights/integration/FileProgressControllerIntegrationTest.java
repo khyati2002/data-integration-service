35f04575b0b6dd53bde02af6381acf9c8e3a94b8
Gaurav Kumar Gupta <gaurav.kumar@salescode.ai>
2025-05-15 07:54:17 +0530

    Update FileProgressControllerIntegrationTest.java

--- insights/src/test/java/com/salescode/dis/insights/integration/FileProgressControllerIntegrationTest.java
+++ insights/src/test/java/com/salescode/dis/insights/integration/FileProgressControllerIntegrationTest.java
@@ -14,6 +14,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.context.annotation.Profile;
 import org.springframework.http.*;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.KafkaContainer;
@@ -30,7 +31,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Testcontainers
-@Profile("test")
+@ActiveProfiles({"postgres", "dev", "debug", "kafka", "test"})
 public class FileProgressControllerIntegrationTest {
 
     @Container
