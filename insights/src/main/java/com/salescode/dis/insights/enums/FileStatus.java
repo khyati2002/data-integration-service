173ddf8bbef65c17361d83a343b1d22f3e7ab95d
Gaurav Kumar Gupta <gaurav.kumar@salescode.ai>
2025-05-19 17:31:01 +0530

    moved to insights-common

--- insights/src/main/java/com/salescode/dis/insights/enums/FileStatus.java
+++
@@ -1,5 +0,0 @@
-package com.salescode.dis.insights.enums;
-
-public enum FileStatus {
-    PENDING, RUNNING, COMPLETED, FAILED
-}
--- insights/src/main/java/com/salescode/dis/insights/enums/JobStatus.java
+++
@@ -1,8 +0,0 @@
-package com.salescode.dis.insights.enums;
-
-public enum JobStatus {
-    PENDING,
-    COMPLETED,
-    FAILED,
-    ABORTED
-}
