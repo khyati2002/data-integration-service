0f218b7cf3f6fa4f61b2f84fd3fabe2423011682
Gaurav Kumar Gupta <gaurav.kumar@salescode.ai>
2025-06-06 10:41:45 +0530

    Update InsightsEnabledFilter.java

--- insights/src/main/java/com/salescode/dis/insights/filter/InsightsEnabledFilter.java
+++ insights/src/main/java/com/salescode/dis/insights/filter/InsightsEnabledFilter.java
@@ -22,7 +22,7 @@ public class InsightsEnabledFilter extends OncePerRequestFilter {
                                     HttpServletResponse response,
                                     FilterChain filterChain) throws ServletException, IOException {
 
-        if(!request.getRequestURI().startsWith("/api")) {
+        if(!request.getRequestURI().startsWith("/api/") || !request.getRequestURI().contains("master") || !request.getRequestURI().contains("job")) {
             filterChain.doFilter(request, response);
             return;
         }
