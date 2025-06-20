a0c2bd0704a7f6e7529a36237bae4d4d002f8330
Prateek Garg <prateekgarg2208@gmail.com>
2025-06-20 15:18:12 +0530

    stock service added

---
+++ src/main/java/com/applicate/services/channelkart/services/StockService.java
@@ -0,0 +1,41 @@
+package com.applicate.services.channelkart.services;
+
+
+import com.applicate.services.channelkart.utils.SecurityContextUtils;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.salescode.dim.jooq.impl.StockIntegration;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+
+import java.io.IOException;
+import java.util.Collection;
+
+public class StockService extends AbstractCDMService<StockIntegration>{
+    private transient HttpClient httpClient;
+
+    @Override
+    public Collection<StockIntegration> batchSave(Collection<StockIntegration> cdmObject) {
+        if (httpClient == null) {
+            httpClient = HttpClients.createDefault();
+        }
+
+        String url = "https://stocks-uat.salescode.ai/hcheck";
+        HttpPost request = new HttpPost(url);
+        request.setHeader("Authorization", "Bearer ");
+        request.setHeader("lob", SecurityContextUtils.getLob());
+        try {
+            String json = new ObjectMapper().writeValueAsString(cdmObject);
+            request.setEntity(new StringEntity(json, ContentType.APPLICATION_JSON));
+            httpClient.execute(request);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return cdmObject;
+    }
+
+}
---
+++ src/main/java/com/salescode/dim/jooq/impl/StockIntegration.java
@@ -0,0 +1,150 @@
+package com.salescode.dim.jooq.impl;
+
+import com.applicate.services.channelkart.models.CommonDataModel;
+import com.applicate.services.channelkart.models.enums.ActiveStatus;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.JsonNode;
+
+import java.time.LocalDateTime;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class StockIntegration extends CommonDataModel {
+
+    private String skuCode;
+    private Integer caseQty;
+    private Integer pieceQty;
+    private Integer otherQty;
+    private String batchId;
+
+    public StockIntegration(String skuCode, Integer caseQty, Integer pieceQty, Integer otherQty, String batchId) {
+        super();
+        this.skuCode = skuCode;
+        this.caseQty = caseQty;
+        this.pieceQty = pieceQty;
+        this.otherQty = otherQty;
+        this.batchId = batchId;
+    }
+
+
+    @Override
+    public String getId() {
+        return "";
+    }
+
+    @Override
+    public void setId(String id) {
+
+    }
+
+    @Override
+    public Integer getVersion() {
+        return 0;
+    }
+
+    @Override
+    public void setVersion(Integer version) {
+
+    }
+
+    @Override
+    public ActiveStatus getActiveStatus() {
+        return null;
+    }
+
+    @Override
+    public void setActiveStatus(ActiveStatus activeStatus) {
+
+    }
+
+    @Override
+    public String getActiveStatusReason() {
+        return "";
+    }
+
+    @Override
+    public void setActiveStatusReason(String activeStatusReason) {
+
+    }
+
+    @Override
+    public LocalDateTime getCreationTime() {
+        return null;
+    }
+
+    @Override
+    public void setCreationTime(LocalDateTime creationTime) {
+
+    }
+
+    @Override
+    public LocalDateTime getLastModifiedTime() {
+        return null;
+    }
+
+    @Override
+    public void setLastModifiedTime(LocalDateTime lastModifiedTime) {
+
+    }
+
+    @Override
+    public String getCreatedBy() {
+        return "";
+    }
+
+    @Override
+    public void setCreatedBy(String createdBy) {
+
+    }
+
+    @Override
+    public String getModifiedBy() {
+        return "";
+    }
+
+    @Override
+    public void setModifiedBy(String modifiedBy) {
+
+    }
+
+    @Override
+    public String getLob() {
+        return "";
+    }
+
+    @Override
+    public void setLob(String lob) {
+
+    }
+
+    @Override
+    public String getSource() {
+        return "";
+    }
+
+    @Override
+    public void setSource(String source) {
+
+    }
+
+    @Override
+    public JsonNode getExtendedAttributes() {
+        return null;
+    }
+
+    @Override
+    public void setExtendedAttributes(JsonNode extendedAttributes) {
+
+    }
+
+    @Override
+    public String getHash() {
+        return "";
+    }
+
+    @Override
+    public void setHash(String hash) {
+
+    }
+}
