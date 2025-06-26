74f461f263b37721a7310632b242d4829ab410ba
khyati12arora <khyati.arora@salescode.ai>
2025-06-26 12:06:31 +0530

    updates

--- insights/src/main/java/com/salescode/dis/insights/enums/ModeOfIntegration.java
+++
@@ -1,7 +0,0 @@
-package com.salescode.dis.insights.enums;
-
-public enum ModeOfIntegration {
-    CK_FILE,
-    CK_API,
-    CK_API_CLIENT
-} 
--- insights/src/main/java/com/salescode/dis/insights/enums/ProgressStage.java
+++
@@ -1,9 +0,0 @@
-package com.salescode.dis.insights.enums;
-
-public enum ProgressStage {
-    READ,
-    PUBLISH,
-    QUEUE,
-    PROCESS,
-    SAVE
-}
