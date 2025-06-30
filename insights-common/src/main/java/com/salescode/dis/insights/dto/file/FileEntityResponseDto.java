4740e12206ef87cff4982cadb040a8a3eed6dd9f
khyati12arora <khyati.arora@salescode.ai>
2025-06-30 12:28:51 +0530

    Revert "Update FileEntityResponseDto.java"
    This reverts commit c411f1404018f9697f073b6ace50927218b7bc4f.

--- insights-common/src/main/java/com/salescode/dis/insights/dto/file/FileEntityResponseDto.java
+++ insights-common/src/main/java/com/salescode/dis/insights/dto/file/FileEntityResponseDto.java
@@ -3,7 +3,6 @@ package com.salescode.dis.insights.dto.file;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.salescode.dis.insights.dto.file.stage.FileStageMetricsDto;
 import com.salescode.dis.insights.enums.ModeOfIntegration;
-import com.salescode.dis.insights.enums.ProgressStatus;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -22,7 +21,7 @@ public class FileEntityResponseDto implements Serializable {
     String id;
     String fileId;
     String master;
-    ProgressStatus status;
+    String status;
     Instant creationTime;
     Instant lastModifiedTime;
     String lob;
