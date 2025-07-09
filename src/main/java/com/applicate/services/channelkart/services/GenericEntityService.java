d85c3984d9b7ae5460242c309902a6e44de41d06
Anushika Kakkar <anushika.kakkar@salescode.ai>
2025-07-09 19:36:22 +0530

    Update GenericEntityService.java

--- src/main/java/com/applicate/services/channelkart/services/GenericEntityService.java
+++ src/main/java/com/applicate/services/channelkart/services/GenericEntityService.java
@@ -24,7 +24,9 @@ public class GenericEntityService extends AbstractCDMService<GenericEntity> {
 		List<List<GenericEntity>> result = new ArrayList<>();
 		List<String> outletCodes = genericEntityList.stream().map(GenericEntity::getId).collect(Collectors.toList());
 
-		Map<String, GenericEntity> savedList = getDslContext().selectFrom(CK_GENERIC_OBJECT).where(CK_GENERIC_OBJECT.ID.in(outletCodes)).fetch().intoMap(CK_GENERIC_OBJECT.ID, record -> record.into(GenericEntity.class));
+//		Map<String, GenericEntity> savedList = getDslContext().selectFrom(CK_GENERIC_OBJECT).where(CK_GENERIC_OBJECT.ID.in(outletCodes)).fetch().intoMap(CK_GENERIC_OBJECT.ID, record -> record.into(GenericEntity.class));
+		Map<String, GenericEntity> savedList = getDslContext().selectFrom(CK_GENERIC_OBJECT).where(CK_GENERIC_OBJECT.ID.in(outletCodes)).fetch().intoMap(CK_GENERIC_OBJECT.ID, record -> convertToGenericEntity(record));
+
 		List<GenericEntity> itemsToInsert = new ArrayList<>();
 		List<GenericEntity> itemsToUpdate = new ArrayList<>();
 		for (GenericEntity loginId : genericEntityList) {
@@ -42,6 +44,7 @@ public class GenericEntityService extends AbstractCDMService<GenericEntity> {
 				GenericEntity existingOutlet = savedList.get(loginId.getId());
 				loginId.setOperationPerformed(ActionType.UPDATE);
 				loginId.setRangeKey(0L);
+				loginId.setChanged((byte)1);
 				loginId.setTimestamp(new Date().toInstant().toEpochMilli());
 				itemsToUpdate.add(loginId);
 			}
@@ -50,6 +53,19 @@ public class GenericEntityService extends AbstractCDMService<GenericEntity> {
 		result.add(itemsToUpdate);
 		return result;
 	}
+
+	private GenericEntity convertToGenericEntity(CkGenericObjectRecord record) {
+		GenericEntity entity = new GenericEntity();
+		entity.setId(record.getId());
+		entity.setRangeKey(record.getRangeKey());
+		entity.setTimestamp(record.getTimestamp());
+		entity.setChanged((byte)1);
+		entity.setActiveStatus(record.getActiveStatus());
+
+
+		return entity;
+	}
+
 
 	@Override
 	public Collection<GenericEntity> batchSave(Collection<GenericEntity> genericEntityList) {
