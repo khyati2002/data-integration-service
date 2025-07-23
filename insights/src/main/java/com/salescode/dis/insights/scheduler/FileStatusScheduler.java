61a25b991c27d799366a2db722a646ef6798cba2
khyati12arora <khyati.arora@salescode.ai>
2025-07-24 03:09:38 +0530

    fixed status scheduler to get last stage status and update

--- insights/src/main/java/com/salescode/dis/insights/scheduler/FileStatusScheduler.java
+++ insights/src/main/java/com/salescode/dis/insights/scheduler/FileStatusScheduler.java
@@ -2,12 +2,16 @@ package com.salescode.dis.insights.scheduler;
 
 import com.salescode.dis.insights.entity.FileEntity;
 import com.salescode.dis.insights.entity.FileStageMetrics;
+import com.salescode.dis.insights.enums.ModeOfIntegration;
+import com.salescode.dis.insights.enums.ProgressStage;
 import com.salescode.dis.insights.enums.ProgressStatus;
 import com.salescode.dis.insights.repository.FileRepository;
 import com.salescode.dis.insights.service.FileService;
 import com.salescode.dis.insights.service.JobService;
+import com.salescode.dis.insights.service.strategy.IFileOperationStrategy;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.flywaydb.core.api.logging.ProgressBar;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -33,6 +37,7 @@ public class FileStatusScheduler {
 
     private final FileRepository fileRepository;
     private final JobService jobService;
+    private final FileService fileService;
 
 
     @Scheduled(fixedRateString = "${file-status-scheduler.rate-millis:60000}") // Run every 1 minute (60000 ms)
@@ -41,10 +46,9 @@ public class FileStatusScheduler {
         log.info("Starting scheduled update of all file statuses");
 
         // Define the time window for staleness
-        Instant staleCutoffTime = Instant.now().minus(STALE_THRESHOLD_SECONDS, ChronoUnit.SECONDS); // e.g., 10 mins ago
-        Instant tooOldCutoffTime = Instant.now().minus(TOO_OLD_THRESHOLD_SECONDS, ChronoUnit.SECONDS); // e.g., 15 mins ago
+        Instant staleCutoffTime = Instant.now().minus(STALE_THRESHOLD_SECONDS, ChronoUnit.SECONDS);
+        Instant tooOldCutoffTime = Instant.now().minus(TOO_OLD_THRESHOLD_SECONDS, ChronoUnit.SECONDS);
 
-        // Find all files that are PENDING and haven't been modified in the 10-15 min window
         // todo remove the check on status as PENDING
         List<FileEntity> pendingFiles = fileRepository.findAllStalePendingFiles(staleCutoffTime, tooOldCutoffTime, ProgressStatus.PENDING);
 
@@ -54,7 +58,6 @@ public class FileStatusScheduler {
         }
 
         log.warn("Found {} potentially stale files (PENDING, modified between {}-{} mins ago). Marking as FAILED.", pendingFiles.size(), STALE_THRESHOLD_SECONDS, TOO_OLD_THRESHOLD_SECONDS);
-
         log.info("Found {} files with PENDING status modified in the last 10 minutes", pendingFiles.size());
 
         for (FileEntity file : pendingFiles) {
@@ -63,11 +66,17 @@ public class FileStatusScheduler {
     }
 
     private void updateFileStatus(FileEntity file) {
-        ProgressStatus lastStageStatus = file.getFileStageMetrics().get(file.getFileStageMetrics().size()-1).getProgressStatus();
+        IFileOperationStrategy strategy = fileService.getFileOperationStrategy(file.getModeOfIntegration());
+        ProgressStage lastStage = strategy.getSupportedStages().getLast();
+        ProgressStatus lastStageStatus = file.getFileStageMetrics()
+                .stream()
+                .filter(metric -> metric.getStageType() == lastStage)
+                .findFirst()
+                .map(FileStageMetrics::getProgressStatus)
+                .orElse(ProgressStatus.PENDING);
         file.setStatus(lastStageStatus);
         file.setEndTime(Instant.now());
         jobService.recalcStatus(file.getJob());
         log.info("Updated file status and jobStatus " + file.getId());
-        }
-
+    }
 }
