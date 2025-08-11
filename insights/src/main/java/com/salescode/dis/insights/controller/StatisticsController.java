d803fd8de2f5882786d760dfe9d9bc1c7df389b9
Aakash Patel <aakashpatel315@gmail.com>
2025-08-11 20:55:21 +0530

    update path

--- insights/src/main/java/com/salescode/dis/insights/controller/StatisticsController.java
+++ insights/src/main/java/com/salescode/dis/insights/controller/StatisticsController.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("")
+@RequestMapping("/api")
 public class StatisticsController {
 
     private final KafkaStatsService kafkaService;
