432d45954479719f34401e17e13b11985c14c629
Aakash Patel <aakashpatel315@gmail.com>
2025-08-11 19:35:13 +0530

    update path

--- insights/src/main/java/com/salescode/dis/insights/config/HttpSecurityConfiguration.java
+++ insights/src/main/java/com/salescode/dis/insights/config/HttpSecurityConfiguration.java
@@ -40,7 +40,7 @@ public class HttpSecurityConfiguration {
                 .cors(cors -> cors.configurationSource(corsConfigurationSource))
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(requests -> requests
-                        .requestMatchers("/hckeck", "/status","/api/properties/**","/api/modes","integration-stats").permitAll()
+                        .requestMatchers("/hckeck", "/status","/api/properties/**","/api/modes","/api/integration-stats").permitAll()
                         .anyRequest().authenticated()
                 );
         return salesCodeAuthManager.build(http);
