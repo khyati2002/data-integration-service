b4e11bd95d006df444d71c5258ef10ecf0856e4c
VedantChauhan-dev <vedant.chauhan@salescode.ai>
2025-08-12 19:27:38 +0530

    changed byte

--- src/main/java/com/applicate/services/channelkart/services/OutletDetailsService.java
+++ src/main/java/com/applicate/services/channelkart/services/OutletDetailsService.java
@@ -224,7 +224,7 @@ public class OutletDetailsService extends AbstractCDMService<OutletDetails> {
             if (savedList.get(outlet.getOutletcode()) == null) {
                 outlet.setVersion(0);
                 outlet.setId(UUID.randomUUID().toString());
-                outlet.setChanged((byte)1);
+                outlet.setChanged(1==1);
                 itemsToInsert.add(outlet);
                 outlet.setOperationPerformed(ActionType.INSERT);
             } else {
@@ -238,7 +238,7 @@ public class OutletDetailsService extends AbstractCDMService<OutletDetails> {
                 if (!Objects.equals(outlet.getHash(), existingOutlet.getHash())) {
                     outlet.setChanges(CdmDiffUtil.getChanges(outlet,existingOutlet));
                     outlet.setOperationPerformed(ActionType.UPDATE);
-                    outlet.setChanged((byte)1);
+                    outlet.setChanged(1==1);
                     itemsToUpdate.add(outlet);
                 }
             }
