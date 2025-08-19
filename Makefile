ad2c5523d5e329521ca6c63292a306380fbebc57
khyati12arora <khyati.arora@salescode.ai>
2025-08-19 14:16:46 +0530

    added FileProgressEvent to insights-commons

--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@
 # Common variables
 # =============================
 profile    ?= default
-version    ?= 0.0.8-SNAPSHOT
+version    ?= 0.0.9-SNAPSHOT
 insights_version ?=
 debug      ?= false
 
---
+++ insights-common/src/main/java/com/salescode/dis/insights/dto/event/FileProgressEvent.java
@@ -0,0 +1,22 @@
+package com.salescode.dis.insights.dto.event;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.salescode.dis.insights.dto.file.progress.FileProgressRequest;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@NoArgsConstructor
+@AllArgsConstructor
+public class FileProgressEvent {
+    private String eventId;
+    private String fileId;
+    private String jobId;
+    private String lob;
+    private String masterName;
+    private FileProgressRequest progress;
+    private long timestamp = System.currentTimeMillis();
+    private String errorMessage;
+}
