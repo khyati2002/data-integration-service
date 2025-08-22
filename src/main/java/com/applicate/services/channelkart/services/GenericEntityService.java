880f21607f84c43f02efc3bfde55ff358ec87cc9
VedantChauhan-dev <vedant.chauhan@salescode.ai>
2025-08-22 11:12:59 +0530

    changed parameter for setChanged()

--- src/main/java/com/applicate/services/channelkart/services/GenericEntityService.java
+++ src/main/java/com/applicate/services/channelkart/services/GenericEntityService.java
@@ -43,7 +43,7 @@ public class GenericEntityService extends AbstractCDMService<GenericEntity> {
 				GenericEntity existingOutlet = savedList.get(loginId.getId());
 				loginId.setOperationPerformed(ActionType.UPDATE);
 				loginId.setRangeKey(0L);
-				loginId.setChanged(1==1);
+				loginId.setChanged(Boolean.TRUE);
 				loginId.setTimestamp(new Date().toInstant().toEpochMilli());
 				itemsToUpdate.add(loginId);
 			}
@@ -58,7 +58,7 @@ public class GenericEntityService extends AbstractCDMService<GenericEntity> {
 		entity.setId(record.getId());
 		entity.setRangeKey(record.getRangeKey());
 		entity.setTimestamp(record.getTimestamp());
-		entity.setChanged(1==1);
+		entity.setChanged(Boolean.TRUE);
 		entity.setActiveStatus(record.getActiveStatus());
 
 
