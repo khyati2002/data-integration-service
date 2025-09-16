8723fc0e7f810db66b7154b7783d6c714aeb27aa
mrinal-sahai-dev <mrinal.sahai@salescode.ai>
2025-09-16 17:44:14 +0530

    update application.yaml

--- insights-sdk/src/main/java/com/salescode/dis/insights/sdk/InsightsEnv.java
+++ insights-sdk/src/main/java/com/salescode/dis/insights/sdk/InsightsEnv.java
@@ -5,7 +5,7 @@ public enum InsightsEnv {
     LOCAL("http://localhost:8081"),
     DEV("https://dis-insights-uat.salescode.ai"),
     UAT("https://dis-insights-uat.salescode.ai"),
-    DEMO("https://dis-insights.salescode.ai"),
+    DEMO("https://dis-insights-demo.salescode.ai"),
     PROD("https://dis-insights.salescode.ai");
 
     private final String insightsUrl;
