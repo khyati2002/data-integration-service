65b44a1030aa91ba205b7dc4a151d601e4da1e65
Chetan <chetan.sethi@salescode.ai>
2025-09-30 14:40:33 +0530

    fix

--- src/main/java/com/applicate/enrichment/HCCBChannelEnrichment.java
+++ src/main/java/com/applicate/enrichment/HCCBChannelEnrichment.java
@@ -83,7 +83,6 @@ public class HCCBChannelEnrichment
             cdm.setSchemeDescription(newDes);
             updateSchemeAndSlabDescription(cdm, pd.getSkuDescription());
             cdm.getSchemeCalculation().get(0).setSchemeDiscountedProductcode(pd.getBatchCode());
-            cdm.getSchemeCalculation().get(0).setSchemeDiscountedProductPrice("1");
         } else {
             logger.error("No product details found for code: {}", inputCode);
         }
