1703675b9531035023e26c1866da363c5aab9cbd
Pranav Arora <pranav.arora@salescode.ai>
2025-10-02 19:11:50 +0530

    Update HCCBTransformer.java

--- src/main/java/com/applicate/transformer/HCCBTransformer.java
+++ src/main/java/com/applicate/transformer/HCCBTransformer.java
@@ -180,7 +180,7 @@ public class HCCBTransformer extends AbstractTransformer<Map<String, Object>, Ma
         schemeCalculationMap.put("minimumAmount", "0");
         schemeCalculationMap.put("usageLimit", "0");
         if(inputMap.get(MONITORING_SCOPE).toString().trim().equalsIgnoreCase("3")) schemeCalculationMap.put("mustBuyGroupId", inputMap.get(SCHEME_NO));
-       com.fasterxml.jackson.databind.node.ObjectNode extendedAttributes = new com.fasterxml.jackson.databind.ObjectMapper().createObjectNode();
+       ObjectNode extendedAttributes = new ObjectMapper().createObjectNode();
         if(inputMap.get(MONITORING_SCOPE).toString().trim().equalsIgnoreCase("3")){
             extendedAttributes.put("mustBuyRepeatSlabSync", "true");
         }
