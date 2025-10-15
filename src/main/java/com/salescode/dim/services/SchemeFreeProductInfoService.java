7f40f3a01ee74bbf672495791923d2dac921bad5
Rajpreet02 <rajpreet.kaur@salescode.ai>
2025-10-15 12:31:38 +0530

    added dynamic unique key support

--- src/main/java/com/salescode/dim/services/SchemeFreeProductInfoService.java
+++ src/main/java/com/salescode/dim/services/SchemeFreeProductInfoService.java
@@ -1,9 +1,12 @@
 package com.salescode.dim.services;
 
 import com.applicate.services.channelkart.services.AbstractCDMService;
+import com.applicate.services.channelkart.services.MetaDataService;
+import com.applicate.services.channelkart.utils.IDGenerator;
 import com.salescode.dim.jooq.generated.tables.pojos.SchemeFreeproductinfo;
 import com.salescode.dim.repository.SchemeFreeProductInfoRepo;
 import com.salescode.dim.repository.SchemeFreeProductInfoRepoImpl;
+import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.JsonNode;
 import org.jooq.DSLContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,10 +24,16 @@ public class SchemeFreeProductInfoService extends AbstractCDMService<SchemeFreep
     private DSLContext dsl = getDslContext();
 
     private SchemeFreeProductInfoRepo schemeFreeProductInfoRepo;
+    private static MetaDataService metaDataService;
+    private static final String DOMAIN_NAME = "Schemefreeproductinfo";
+    private static final String DOMAIN_TYPE = "DynamicUniqueKey";
+    private static IDGenerator idGenerator = null;
 
     public SchemeFreeProductInfoService() {
         this.dsl = getDslContext();
         schemeFreeProductInfoRepo=new SchemeFreeProductInfoRepoImpl(dsl);
+        metaDataService = new MetaDataService();
+        idGenerator = new IDGenerator();
     }
 
     public List<SchemeFreeproductinfo> findBySchemeId(String schemeId) {
@@ -36,9 +45,10 @@ public class SchemeFreeProductInfoService extends AbstractCDMService<SchemeFreep
     }
 
     public void sfpSave(List<SchemeFreeproductinfo> bifurcations) {
+        JsonNode metadata = metaDataService.fetchByValue(DOMAIN_NAME, DOMAIN_TYPE).getDomainValues();
         if (bifurcations != null) {
             bifurcations.forEach(sfp -> {
-                sfp.setId(UUID.randomUUID().toString());
+                sfp.setId(idGenerator.getIdWithMetaData(sfp, metadata));
                 sfp.setVersion(0);
                 super.addHash(sfp);
             });
