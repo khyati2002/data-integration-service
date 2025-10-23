c51d83657b8f2266c8993bd16379dd7dc31d4a5c
Aakash Patel <aakash.patel@salescode.ai>
2025-10-23 17:34:13 +0530

    Update FileProgressEventListener.java

--- insights/src/main/java/com/salescode/dis/insights/kafka/FileProgressEventListener.java
+++ insights/src/main/java/com/salescode/dis/insights/kafka/FileProgressEventListener.java
@@ -41,7 +41,7 @@ public class FileProgressEventListener {
     private final ThreadPoolTaskExecutor fileProgressExecutor;
 
 
-    @KafkaListener(topics = "${file.progress.update.topic:file-progress-updates-2}",
+    @KafkaListener(topics = "${file.progress.update.topic:file-progress-updates}",
             groupId = "${file.progress.processor:file-progress-processor}", batch = "true",
             containerFactory = "fileProgressContainerFactory",
             concurrency = "5",
