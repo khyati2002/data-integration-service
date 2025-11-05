3908cd5a9220e98c199d17f2e841674adc29e72e
MananGarg-22 <manan.garg@salescode.ai>
2025-11-05 19:16:20 +0530

    added SupplierMetaDataService file

---
+++ src/main/java/com/applicate/services/channelkart/services/SupplierMetaDataService.java
@@ -0,0 +1,8 @@
+package com.applicate.services.channelkart.services;
+
+import com.salescode.dim.jooq.impl.SupplierMetaData;
+import com.salescode.dim.jooq.impl.User;
+
+public class SupplierMetaDataService extends AbstractCDMService<SupplierMetaData>  {
+
+}
