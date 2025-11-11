c259d675b3a1f8cfaa5e160abb0508685f5bf1d0
samarth-scai <samarth.narayan@salescode.ai>
2025-11-11 17:03:39 +0530

    added generic outlet details enrichment

--- src/main/java/com/applicate/services/channelkart/enrichments/repository/GenericOutletDetailsEnrichment.java
+++ src/main/java/com/applicate/services/channelkart/enrichments/repository/GenericOutletDetailsEnrichment.java
@@ -1,34 +1,29 @@
-/*
-*Copyright Applicate(2021) To Present
-*
-*All rights reserved
-*/
 package com.applicate.services.channelkart.enrichments.repository;
 
-
-
+import com.applicate.services.channelkart.component.model.SequenceGenerator;
+import com.applicate.services.channelkart.converters.DateToClientTimeZoneStringConverter;
 import com.applicate.services.channelkart.models.enums.ActiveStatus;
+import com.applicate.services.channelkart.security.SecurityContextUtils;
+import com.applicate.services.channelkart.services.SequenceInfoService;
+import com.applicate.services.channelkart.services.ServiceLocator;
+import com.salescode.dim.etl.EnrichmentResult;
 import com.salescode.dim.etl.OperationResult;
-import com.salescode.dim.etl.enrichment.AbstractEnrichment;
 import com.salescode.dim.jooq.impl.OutletDetails;
-import org.apache.commons.lang3.StringUtils;
+import com.salescode.dim.jooq.impl.SequenceInfo;
+import org.jooq.tools.StringUtils;
 
-/**
- * The class GenericOutletDetailsEnrichment.
- *
- * @author  Manish Srivastava
- * @since   Feb 2021
- */
-public class GenericOutletDetailsEnrichment extends AbstractEnrichment<OutletDetails> {
+import java.util.Date;
 
+public class GenericOutletDetailsEnrichment {
+
+	private SequenceInfoService sequenceService= (SequenceInfoService) ServiceLocator.lookup(SequenceInfo.class);
 	/**
 	 * Apply.
 	 *
 	 * @param cdm the cdm
 	 * @return the enrichment result
 	 */
-	@Override
-	public OperationResult.StepResult apply(OutletDetails cdm) {
+	public EnrichmentResult apply(OutletDetails cdm) {
 
 		if(cdm.getActiveStatus().equals(ActiveStatus.ACTIVE)) {
 			cdm.setActiveStatus(ActiveStatus.ACTIVE);
@@ -38,29 +33,28 @@ public class GenericOutletDetailsEnrichment extends AbstractEnrichment<OutletDet
 			}
 		}else {
 			cdm.setActiveStatus(ActiveStatus.INACTIVE);
-			setActiveStatusReasonForInactiveOutlet(cdm);			
+			setActiveStatusReasonForInactiveOutlet(cdm);
 		}
 
 		if(cdm.getMapped() == null)
 			cdm.setMapped(Boolean.FALSE);
 
-//		/*Sequence Generator*/
-//		if(sequenceService.shouldEnableSequenceGenerator(cdm.getClass().getSimpleName(), "outletcode",cdm.getOutletcode())) {
-//			cdm.setOutletcode(SequenceGenerator.Value.STRING.getDefaultValue());
-//		}
+		/*Sequence Generator*/
+		if(sequenceService.shouldEnableSequenceGenerator(cdm.getClass().getSimpleName(), "outletCode",cdm.getOutletcode())) {
+			cdm.setOutletCode(SequenceGenerator.Value.STRING.getDefaultValue());
+		}
 
 		return new OperationResult.StepResult(OperationResult.Status.OK,"Data enriched successfully");
 
 	}
-	
+
 	private void setActiveStatusReasonForInactiveOutlet(OutletDetails cdm) {
 		if(!StringUtils.isBlank(cdm.getActiveStatusReason()) && cdm.getActiveStatusReason().startsWith("REJECTED")) {
-			   cdm.setActiveStatusReason("REJECTED");
-		   }else {
-			   cdm.setActiveStatusReason("Deactivated by "); //+ SecurityContextUtils.getPrincipal()+
-//						" on "+ new DateToClientTimeZoneStringConverter().convert(new Date()));
-		   }
-	
-	}
+			cdm.setActiveStatusReason("REJECTED");
+		}else {
+			cdm.setActiveStatusReason("Deactivated by "+ SecurityContextUtils.getPrincipal()+
+					" on "+ new DateToClientTimeZoneStringConverter().convert(new Date()));
+		}
 
+	}
 }
