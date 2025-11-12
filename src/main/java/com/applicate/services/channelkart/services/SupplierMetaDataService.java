0f6b466db9808a754f3b17b492aed6173a7a29dd
Anushika Kakkar <anushika.kakkar@salescode.ai>
2025-11-12 14:27:11 +0530

    updated supplierMetadata Service

--- src/main/java/com/applicate/services/channelkart/services/SupplierMetaDataService.java
+++ src/main/java/com/applicate/services/channelkart/services/SupplierMetaDataService.java
@@ -1,14 +1,17 @@
 package com.applicate.services.channelkart.services;
 
-
+import com.applicate.services.channelkart.models.enums.ActionType;
 import com.applicate.services.channelkart.utils.IdGenerator;
 import com.salescode.dim.jooq.generated.tables.records.CkSupplierMetadataRecord;
+import com.salescode.dim.jooq.generated.tables.records.CkUserMetadataRecord;
 import com.salescode.dim.jooq.impl.SupplierMetadata;
+import com.salescode.dim.jooq.impl.UserMetadata;
+
 
 import java.util.*;
 import java.util.stream.Collectors;
-
 import static com.salescode.dim.jooq.generated.Tables.CK_SUPPLIER_METADATA;
+
 
 public class SupplierMetaDataService extends AbstractCDMService<SupplierMetadata> {
 
@@ -16,6 +19,7 @@ public class SupplierMetaDataService extends AbstractCDMService<SupplierMetadata
 		if (supplierMetaDataList == null || supplierMetaDataList.isEmpty()) {
 			return;
 		}
+		supplierMetaDataList.forEach((supplierMetadata -> supplierMetadata.setId(userLoginId)));
 
 		// Find existing supplier metadata for this user
 		List<SupplierMetadata> existingSupplierMetaData = findByUserLoginId(userLoginId);
@@ -23,8 +27,43 @@ public class SupplierMetaDataService extends AbstractCDMService<SupplierMetadata
 		// Filter only new/different items that need to be saved
 		List<SupplierMetadata> itemsToSave = supplierMetaDataList.stream().filter(newItem -> !hasMatchingItem(newItem, existingSupplierMetaData)).collect(Collectors.toList());
 
-
+		getItemsToSaveList(supplierMetaDataList);
 		batchSave(itemsToSave);
+	}
+
+	public List<List<SupplierMetadata>> getItemsToSaveList(List<SupplierMetadata> supplierMetadataList) {
+		List<List<SupplierMetadata>> result = new ArrayList<>();
+		List<String> users = supplierMetadataList.stream().map(SupplierMetadata::getId).collect(Collectors.toList());
+
+		Map<String, com.salescode.dim.jooq.generated.tables.pojos.SupplierMetadata> savedList = getDslContext().selectFrom(CK_SUPPLIER_METADATA).where(CK_SUPPLIER_METADATA.ID.in(users)).fetch().intoMap(CK_SUPPLIER_METADATA.ID, record -> record.into(com.salescode.dim.jooq.generated.tables.pojos.SupplierMetadata.class));
+
+		List<SupplierMetadata> itemsToInsert = new ArrayList<>();
+		List<SupplierMetadata> itemsToUpdate = new ArrayList<>();
+		for (SupplierMetadata loginid : supplierMetadataList) {
+			fillAttributes(loginid, savedList.get(loginid.getId()));
+			fillCommonAttributes(loginid);
+			if (loginid.getId() == null)
+				loginid.setId(new IdGenerator(loginid.getClass().getSimpleName()).getId(loginid));
+
+			if (savedList.get(loginid.getId()) == null) {
+				// New record -> assign ID
+				loginid.setId(new IdGenerator(loginid.getClass().getSimpleName()).getId(loginid));
+				itemsToInsert.add(loginid);
+				loginid.setOperationPerformed(ActionType.INSERT);
+				loginid.setVersion(0);
+			} else {
+				// Existing record -> do NOT change ID
+				com.salescode.dim.jooq.generated.tables.pojos.SupplierMetadata existingUser = savedList.get(loginid.getId());
+				loginid.setId(existingUser.getId()); // Preserve existing ID
+				loginid.setOperationPerformed(ActionType.UPDATE);
+				loginid.setChanged(Boolean.TRUE);
+				loginid.setVersion(existingUser.getVersion() + 1);
+				itemsToUpdate.add(loginid);
+			}
+		}
+		result.add(itemsToInsert);
+		result.add(itemsToUpdate);
+		return result;
 	}
 
 
@@ -41,24 +80,57 @@ public class SupplierMetaDataService extends AbstractCDMService<SupplierMetadata
 	}
 
 	@Override
-	public List<SupplierMetadata> batchSave(Collection<SupplierMetadata> supplierMetaDataCollection) {
-		List<SupplierMetadata> supplierMetaDataList = new ArrayList<>(supplierMetaDataCollection);
+	public Collection<SupplierMetadata> batchSave(Collection<SupplierMetadata> supplierMetadataList) {
+//		LOG.info("Size of list is {}", supplierMetadataList.size());
 
-		if (supplierMetaDataList.isEmpty()) {
-			return supplierMetaDataList;
+		try {
+			List<List<SupplierMetadata>> saveItemsList = getItemsToSaveList(new ArrayList<>(supplierMetadataList));
+
+			if (!saveItemsList.get(0).isEmpty()) {
+			//	LOG.info("Performing batch insert for {} records", saveItemsList.get(0).size());
+
+				List<CkSupplierMetadataRecord> insertRecords = saveItemsList.get(0).stream().map(supplierMetadata -> {
+					CkSupplierMetadataRecord record = getDslContext().newRecord(CK_SUPPLIER_METADATA, supplierMetadata);
+					return record;
+				}).collect(Collectors.toList());
+
+				getDslContext().batchInsert(insertRecords).execute();
+			}
+
+			if (!saveItemsList.get(1).isEmpty()) {
+
+				List<CkSupplierMetadataRecord> updateRecords = saveItemsList.get(1).stream().map(supplierMetadata -> {
+					CkSupplierMetadataRecord record = getDslContext().newRecord(CK_SUPPLIER_METADATA, supplierMetadata);
+
+
+					return record;
+				}).collect(Collectors.toList());
+
+				getDslContext().batchUpdate(updateRecords).execute();
+			}
+
+		} catch (Exception e) {
+
 		}
-
-		// All items are new since we've already filtered for differences
-		getDslContext().batchInsert(supplierMetaDataList.stream().map(supplier -> {
-//                                supplier.setId(UUID.randomUUID().toString());
-			supplier.setId(new IdGenerator(supplier.getClass().getSimpleName()).getId(supplier));
-			supplier.setChanged(true);
-			CkSupplierMetadataRecord record = getDslContext().newRecord(CK_SUPPLIER_METADATA, supplier);
-			return record;
-		}).collect(Collectors.toList())).execute();
-
-
-		return supplierMetaDataList;
+		return supplierMetadataList;
 	}
+//	@Override
+//	public List<SupplierMetadata> batchSave(Collection<SupplierMetadata> supplierMetaDataCollection) {
+//		List<SupplierMetadata> supplierMetaDataList = new ArrayList<>(supplierMetaDataCollection);
+//
+//		if (supplierMetaDataList.isEmpty()) {
+//			return supplierMetaDataList;
+//		}
+//		// All items are new since we've already filtered for differences
+//		getDslContext().batchInsert(supplierMetaDataList.stream().map(supplier -> {
+////			supplier.setId(new IdGenerator(supplier.getClass().getSimpleName()).getId(supplier));
+//			supplier.setChanged(true);
+//			CkSupplierMetadataRecord record = getDslContext().newRecord(CK_SUPPLIER_METADATA, supplier);
+//			return record;
+//		}).collect(Collectors.toList())).execute();
+//
+//
+//		return supplierMetaDataList;
+//	}
 
 }
