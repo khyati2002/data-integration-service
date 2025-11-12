f981ec7ef677ebb3c8294d1bb779234f55a0b0db
Anushika Kakkar <anushika.kakkar@salescode.ai>
2025-11-12 17:36:48 +0530

    Update SupplierMetaDataService.java

--- src/main/java/com/applicate/services/channelkart/services/SupplierMetaDataService.java
+++ src/main/java/com/applicate/services/channelkart/services/SupplierMetaDataService.java
@@ -63,7 +63,7 @@ public class SupplierMetaDataService extends AbstractCDMService<SupplierMetadata
 			if (savedList.get(outlet.getId()) == null) {
 				outlet.setVersion(0);
 				outlet.setId(UUID.randomUUID().toString());
-				outlet.setChanged(1==1);
+				outlet.setChanged(Boolean.TRUE);
 				itemsToInsert.add(outlet);
 				outlet.setOperationPerformed(ActionType.INSERT);
 			} else {
@@ -77,7 +77,7 @@ public class SupplierMetaDataService extends AbstractCDMService<SupplierMetadata
 				if (!Objects.equals(outlet.getHash(), existingOutlet.getHash())) {
 					outlet.setChanges(CdmDiffUtil.getChanges(outlet,existingOutlet));
 					outlet.setOperationPerformed(ActionType.UPDATE);
-					outlet.setChanged(1==1);
+					outlet.setChanged(Boolean.TRUE);
 					itemsToUpdate.add(outlet);
 				}
 			}
