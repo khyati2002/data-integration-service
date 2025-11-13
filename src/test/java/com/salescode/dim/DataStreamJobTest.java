8d12218112f115d2b099a5e6e225e45fdcd0eab5
samarth-scai <samarth.narayan@salescode.ai>
2025-11-13 12:49:08 +0530

    pushed application.properties and data test

--- src/test/java/com/salescode/dim/DataStreamJobTest.java
+++ src/test/java/com/salescode/dim/DataStreamJobTest.java
@@ -37,48 +37,139 @@ class CollectSink<T> implements SinkFunction<T> {
 public class DataStreamJobTest {
 
     public static String rawStreamingData = "{\n" +
-            "    \"requestId\": \"70egrh7c-8bcb-4d51-a7a6-293a2b0d8e4c\",\n" +
-            "    \"groupId\": \"2025-11-11\",\n" +
-            "    \"fileId\": null,\n" +
-            "    \"lob\": \"itcvissfainuat\",\n" +
-            "    \"submittedBy\": null,\n" +
-            "    \"transformerInfo\": [\n" +
-            "        {\n" +
-            "            \"entityName\": \"Targets\",\n" +
-            "            \"operationType\": \"insert\",\n" +
-            "            \"transformerId\": \"mdm_targets_transformer\"\n" +
-            "        }\n" +
-            "    ],\n" +
-            "    \"topicName\": null,\n" +
-            "    \"preserveOnFailure\": true,\n" +
-            "    \"features\": [\n" +
-            "        {\n" +
-            "            \"MONTH\": \"Nov\",\n" +
-            "            \"YEAR\": \"2025\",\n" +
-            "            \"PSRID\": \"4002883\",\n" +
-            "            \"DISTRICT\": \"WDIS\",\n" +
-            "            \"BRANCH\": \"WPUN\",\n" +
-            "            \"PARAMETER\": \"SSPSS Gate Numeric Ach\",\n" +
-            "            \"PRODUCT_LEVEL\": null,\n" +
-            "            \"FOCUS\": \"No\",\n" +
-            "            \"TARGET\": \"44.000000\",\n" +
-            "            \"ACH\": \"8.000000\",\n" +
-            "            \"ACH_PER\": \"18.180000\",\n" +
-            "            \"FOCUS_DESC\": null,\n" +
-            "            \"ALLOCATED_POINTS\": null,\n" +
-            "            \"EARNED_POINTS\": null,\n" +
-            "            \"MIN_SLAB_FROM\": null,\n" +
-            "            \"MAX_SLAB_FROM\": null,\n" +
-            "            \"MIN_SLAB_PAYOUT\": null,\n" +
-            "            \"MAX_SLAB_PAYOUT\": null\n" +
-            "        }\n" +
-            "    ],\n" +
-            "    \"loginId\": \"integration_user\",\n" +
-            "    \"offset\": null,\n" +
-            "    \"retryCount\": null,\n" +
-            "    \"ignoreS3Log\": false,\n" +
-            "    \"headersMap\": null\n" +
+            "  \"requestId\": \"ed5c317c-334ge-b07a-63355ef78cac\",\n" +
+            "  \"groupId\": \"2025-11-12\",\n" +
+            "  \"fileId\": null,\n" +
+            "  \"lob\": \"itcvissfainuat\",\n" +
+            "  \"submittedBy\": null,\n" +
+            "  \"transformerInfo\": [\n" +
+            "    {\n" +
+            "      \"entityName\": \"ProductMetadata\",\n" +
+            "      \"transformerId\": \"mdm_productmetadata_java_integ\",\n" +
+            "      \"operationType\": \"insert\"\n" +
+            "    }\n" +
+            "  ],\n" +
+            "  \"topicName\": null,\n" +
+            "  \"preserveOnFailure\": true,\n" +
+            "  \"features\": [\n" +
+            "    {\n" +
+            "      \"WDDEST\": \"CA3704\",\n" +
+            "      \"SubCatCode\": \"AMNPS\",\n" +
+            "      \"SubCatName\": \"Popular Small Pack\",\n" +
+            "      \"SBU\": \"AGARBATTI\",\n" +
+            "      \"MktSkuCode\": \"MD20MOGRA\",\n" +
+            "      \"MktSkuName\": \"MD20MOGRA\",\n" +
+            "      \"SysSkuCode\": \"11590\",\n" +
+            "      \"SysSkuName\": \"Mangaldeep 12 Mogra(MB)\",\n" +
+            "      \"SimpleProductDesc\": \"MD Mogra Agarbatti - Rs 10\",\n" +
+            "      \"CatCode\": \"AG\",\n" +
+            "      \"CatName\": \"AGARBATTI\",\n" +
+            "      \"BrandCode\": \"MN\",\n" +
+            "      \"BrandName\": \"MANGALDEEP\",\n" +
+            "      \"Tax\": \"5.000000\",\n" +
+            "      \"PACPTR\": \"10.333400\",\n" +
+            "      \"CFCPTR\": \"2005.004600\",\n" +
+            "      \"PACIN1CFC\": 240,\n" +
+            "      \"MRP\": \"45.000000\"\n" +
+            "    }\n" +
+            "  ],\n" +
+            "  \"loginId\": \"integration_user\",\n" +
+            "  \"offset\": null,\n" +
+            "  \"retryCount\": null,\n" +
+            "  \"ignoreS3Log\": false,\n" +
+            "  \"headersMap\": null\n" +
             "}";
+
+//    public static String rawStreamingData = "{\n" +
+//            "   \"requestId\": \"782cc28b-f246-4907-8637-cf51ae61139f\",\n" +
+//            "   \"groupId\": \"2025-11-10\",\n" +
+//            "   \"fileId\": null,\n" +
+//            "   \"lob\": \"itcvissfain\",\n" +
+//            "   \"submittedBy\": null,\n" +
+//            "   \"transformerInfo\": [\n" +
+//            "       {\n" +
+//            "           \"entityName\": \"OutletDetails\",\n" +
+//            "           \"transformerId\": \"mdm_stockist_transformer_integ1\",\n" +
+//            "           \"operationType\": \"insert\"\n" +
+//            "       }\n" +
+//            "   ],\n" +
+//            "   \"topicName\": null,\n" +
+//            "   \"preserveOnFailure\": true,\n" +
+//            "   \"features\": [\n" +
+//            "       {\n" +
+//            "           \"District\": \"EDIS\",\n" +
+//            "           \"Branch\": \"EBEN\",\n" +
+//            "           \"PSRCRMID\": \"4001170\",\n" +
+//            "           \"stockistName\": \"EAHAN STORE\",\n" +
+//            "           \"userName\": null,\n" +
+//            "           \"KYC\": \"Y\",\n" +
+//            "           \"STOCKIST_L3M_BAND\": \"PLATINUM\",\n" +
+//            "           \"UID\": \"C20220005437260\",\n" +
+//            "           \"AUS\": \"\",\n" +
+//            "           \"ActiveStatus\": \"ACTIVE\",\n" +
+//            "           \"StockistBand\": \"PLATINUM\",\n" +
+//            "           \"CREATION_DATE\": \"2025-11-10\",\n" +
+//            "           \"PICK_UP_STATUS\": \"0\",\n" +
+//            "           \"LOADDATE\": \"2025-11-10\",\n" +
+//            "           \"BeatID\": null,\n" +
+//            "           \"Beat\": null,\n" +
+//            "           \"COLOUR\": null,\n" +
+//            "           \"supplierMapping\": [\n" +
+//            "               {\n" +
+//            "                   \"CustID\": \"342EXP2023\",\n" +
+//            "                   \"SIFYID\": \"CA3210ITC510342EXP2023\",\n" +
+//            "                   \"WDDest\": \"CA3210\",\n" +
+//            "                   \"UID\": \"C20220005437260\",\n" +
+//            "                   \"RCSID\": \"181204528215\",\n" +
+//            "                   \"WDName\": \" EAHAN STORE\"\n" +
+//            "               }\n" +
+//            "           ],\n" +
+//            "           \"WDDest\": [\n" +
+//            "               \"CA3210\"\n" +
+//            "           ]\n" +
+//            "       }\n" +
+//            "   ],\n" +
+//            "   \"loginId\": \"integration_user\",\n" +
+//            "   \"offset\": null,\n" +
+//            "   \"retryCount\": null,\n" +
+//            "   \"ignoreS3Log\": false,\n" +
+//            "   \"headersMap\": null\n" +
+//            "}";
+//    public static String rawStreamingData = "{\n" +
+//            "    \"requestId\": \"99deb-ererfe543223vsdvf=3e-8581-9vfd70r21232\",\n" +
+//            "    \"groupId\": \"2025-03-22\",\n" +
+//            "    \"lob\": \"itcvissfainuat\",\n" +
+//            "    \"loginId\": \"admin@applicate.in\",\n" +
+//            "    \"batchNumber\": 0,\n" +
+//            "    \"transformerInfo\": [\n" +
+//            "        {\n" +
+//            "            \"skipPreprocessing\": false,\n" +
+//            "            \"skipPersist\": false,\n" +
+//            "            \"entityName\": \"User\",\n" +
+//            "            \"transformerId\": \"mdm_user_psr_integ_test\",\n" +
+//            "            \"preprocessValidationExcludeGroup\": null,\n" +
+//            "            \"messageLevelHash\": \"NUgKG5wpO4h0jfQJoXzBVUUVfX2sPWej4PXSdlD7HRw=\",\n" +
+//            "            \"messageHashSupported\": false,\n" +
+//            "            \"messageLevelKey\": \"mdm_user_psr_integ:User:4002583\",\n" +
+//            "            \"cachedArtifact\": null,\n" +
+//            "            \"operationType\": \"insert\"\n" +
+//            "        }\n" +
+//            "    ],\n" +
+//            "    \"features\": [\n" +
+//            "        {\n" +
+//            "            \"District\": \"EDIS\",\n" +
+//            "            \"Branch\": \"EGAU\",\n" +
+//            "            \"immediateParent\": \"NG22411\",\n" +
+//            "            \"PSRCRMID\": \"6842336\",\n" +
+//            "            \"PSRName\": \"Samarth hcPSR\",\n" +
+//            "            \"userName\": \"6112021545\",\n" +
+//            "            \"DSType\": \"PSR\",\n" +
+//            "            \"AUS\": \"Y\",\n" +
+//            "            \"PICK_UP_STATUS\": \"0\",\n" +
+//            "            \"source_key\": \"integration\"\n" +
+//            "        }\n" +
+//            "    ]\n" +
+//            "}";
 
     @Test
     public void testDataStreamJobWithFewObjects() throws Exception {
@@ -95,7 +186,8 @@ public class DataStreamJobTest {
         StreamingRawData data3 = createStreamingDataObject(Map.of("groupId", "req-3"));
 
         // Create a source from the sample data
-        DataStream<StreamingRawData> source = env.fromData(data1, data2, data3);
+        //DataStream<StreamingRawData> source = env.fromData(data1, data2, data3);
+        DataStream<StreamingRawData> source = env.fromData(data1);
 
         // For testing, we bypass Kafka and directly use the processor.
         // Prepare dummy commonProperties (if needed by StreamingRawDataProcessor)
@@ -104,7 +196,7 @@ public class DataStreamJobTest {
         SingleOutputStreamOperator<Tuple2<StreamingRawData, Map<Class<? extends CommonDataModel>, Set<CommonDataModel>>>> processedStream = AsyncDataStream.unorderedWait(
                 source.rebalance().flatMap(new StreamingRawDataFlatMapper()), // Pre-process data
                 new StreamingRawDataProcessor(stringPropertiesMap.get("Common")),  // Async Processing
-                8000, TimeUnit.SECONDS  // Timeout to prevent blocking indefinitely
+                5000, TimeUnit.SECONDS  // Timeout to prevent blocking indefinitely
         ).process(new ProcessRecordStatus());
 
         processedStream.sinkTo(new JooqDatabaseBatchSink(stringPropertiesMap.get("Common"))).name("Database Success Sink");
