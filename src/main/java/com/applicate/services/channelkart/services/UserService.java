0349980e793e5ecb7e0ab5886b6cf42f5529b8cb
Kartik Rana <kartik.rana@salescode.ai>
2025-11-17 20:58:03 +0530

    Update UserService.java

--- src/main/java/com/applicate/services/channelkart/services/UserService.java
+++ src/main/java/com/applicate/services/channelkart/services/UserService.java
@@ -229,7 +229,7 @@ public class UserService extends AbstractCDMService<User> {
     private void fillUserDetails(List<User> userList) {
         for(User user : userList) {
             if (user.getVerified() == null) {
-                user.setVerified(false);
+                user.setVerified((byte) 0);
             }
 
             if (user.getPassword() == null) {
@@ -291,8 +291,9 @@ public class UserService extends AbstractCDMService<User> {
                     user.setPassword(savedList.get(user.getLoginid()).getPassword());
                 }
             }
-            if (savedList.get(user.getLoginid()) != null && User.of(savedList.get(user.getLoginid())).getVerified()) {
-                user.setVerified(true);
+            if (savedList.get(user.getLoginid()) != null &&
+                    Byte.valueOf((byte)1).equals(User.of(savedList.get(user.getLoginid())).getVerified())) {
+                user.setVerified((byte) 1);
             }
             super.addHash(user);
             if (savedList.get(user.getLoginid()) == null) {
