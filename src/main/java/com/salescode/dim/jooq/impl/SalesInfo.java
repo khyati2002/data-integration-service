ca8d6db3fca488e3dd462b690e43e3bf7f6bc4dc
dibyasundar <dibya.sundar@salescode.ai>
2025-11-17 10:51:01 +0530

    update Salesinfo

--- src/main/java/com/salescode/dim/jooq/impl/SalesInfo.java
+++ src/main/java/com/salescode/dim/jooq/impl/SalesInfo.java
@@ -1,11 +1,13 @@
 package com.salescode.dim.jooq.impl;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
 
 @Data
+@AllArgsConstructor
 public class SalesInfo implements Serializable {
     private BigDecimal gstAmount;
     private BigDecimal tcsAmount;
