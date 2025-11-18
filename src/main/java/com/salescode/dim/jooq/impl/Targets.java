f0a1b9deec3aea8a717d9e7a1fdd4e5887c8465c
MananGarg-22 <manan.garg@salescode.ai>
2025-11-18 11:54:37 +0530

    targets and targetresults

--- src/main/java/com/salescode/dim/jooq/impl/Targets.java
+++ src/main/java/com/salescode/dim/jooq/impl/Targets.java
@@ -5,6 +5,11 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
 import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.annotation.JsonInclude;
+import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import java.time.LocalDateTime;
 
 import java.io.Serializable;
 import java.util.List;
@@ -22,6 +27,34 @@ public class Targets extends com.salescode.dim.jooq.generated.tables.pojos.Targe
 
     public Targets(){
         super();
+    }
+
+    @Override
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    public void setStartDate(LocalDateTime startDate) {
+        super.setStartDate(startDate);
+    }
+
+    @Override
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    public void setEndDate(LocalDateTime endDate) {
+        super.setEndDate(endDate);
+    }
+
+    @Override
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    public void setCreationTime(LocalDateTime creationTime) {
+        super.setCreationTime(creationTime);
+    }
+
+    @Override
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    public void setLastModifiedTime(LocalDateTime lastModifiedTime) {
+        super.setLastModifiedTime(lastModifiedTime);
     }
 
     public List<TargetResults> getTargetResults() {
