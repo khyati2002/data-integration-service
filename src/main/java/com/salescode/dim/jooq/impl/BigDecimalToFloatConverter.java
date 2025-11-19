0f678407afdc56cae8afea3997e659ae93f9a30d
Anushika Kakkar <anushika.kakkar@salescode.ai>
2025-11-19 14:55:35 +0530

    updated productMetadata service code

--- src/main/java/com/salescode/dim/jooq/impl/BigDecimalToFloatConverter.java
+++ src/main/java/com/salescode/dim/jooq/impl/BigDecimalToFloatConverter.java
@@ -2,17 +2,31 @@ package com.salescode.dim.jooq;
 
 import org.jooq.Converter;
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public class BigDecimalToFloatConverter implements Converter<BigDecimal, Float> {
 
 	@Override
 	public Float from(BigDecimal databaseObject) {
-		return databaseObject == null ? null : databaseObject.floatValue();
+		if (databaseObject == null) {
+			return null;
+		}
+
+		// keep DB precision but convert to float (will naturally truncate)
+		return databaseObject.floatValue(); // results in 2322.6047
 	}
 
 	@Override
 	public BigDecimal to(Float userObject) {
-		return userObject == null ? null : BigDecimal.valueOf(userObject);
+		if (userObject == null) {
+			return null;
+		}
+
+		// Convert float -> BigDecimal from exact string representation
+		BigDecimal bd = new BigDecimal(Float.toString(userObject));
+
+		// force to 8 decimal places to match DB
+		return bd.setScale(8, RoundingMode.HALF_UP); // 2322.60470000
 	}
 
 	@Override
