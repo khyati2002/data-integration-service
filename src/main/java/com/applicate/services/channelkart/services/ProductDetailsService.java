834049639e2b920b4811da357035bbc2b80b94f7
Anushika Kakkar <anushika.kakkar@salescode.ai>
2025-11-25 15:21:04 +0530

    review comments fix

--- src/main/java/com/applicate/services/channelkart/services/ProductDetailsService.java
+++ src/main/java/com/applicate/services/channelkart/services/ProductDetailsService.java
@@ -32,7 +32,7 @@ public class ProductDetailsService extends AbstractCDMService<ProductDetails> {
 	public static final String BATCH_CODE_SEPARATOR = "-";
 	private static final Logger LOG = LoggerFactory.getLogger(ProductDetailsService.class);
 	private final List<String> fileNameColumns = Arrays.asList("fileName", "fileName_a", "fileName_b", "fileName_c", "fileName_f", "fileName_l");
-	private  LocationService locationService;
+	private  LocationService locationService=(LocationService) ServiceLocator.lookup(Location.class);
 	private  ProductDetailsRepository productDetailsRepository;
 
 	public ProductDetailsService(){
@@ -43,7 +43,6 @@ public class ProductDetailsService extends AbstractCDMService<ProductDetails> {
 	}
 	public ProductDetailsService(ProductDetailsRepository productDetailsRepository) {
 		this.productDetailsRepository = productDetailsRepository;
-		this.locationService = new LocationService();
 	}
 
 
