79d78e0d21cf96139ed2bf21f024d130958577e8
siddharth-2510 <siddharth.jaiswal@salescode.ai>
2025-11-25 13:32:58 +0530

    comm

--- src/main/java/com/applicate/services/channelkart/services/UserService.java
+++ src/main/java/com/applicate/services/channelkart/services/UserService.java
@@ -229,7 +229,7 @@ public class UserService extends AbstractCDMService<User> {
     private void fillUserDetails(List<User> userList) {
         for(User user : userList) {
             if (user.getVerified() == null) {
-                user.setVerified(false);
+                user.setVerified((byte)0);
             }
 
             if (user.getPassword() == null) {
@@ -302,8 +302,8 @@ public class UserService extends AbstractCDMService<User> {
                     user.setPassword(savedList.get(user.getLoginid()).getPassword());
                 }
             }
-            if (savedList.get(user.getLoginid()) != null && User.of(savedList.get(user.getLoginid())).getVerified()) {
-                user.setVerified(true);
+            if (savedList.get(user.getLoginid()) != null && User.of(savedList.get(user.getLoginid())).getVerified().equals((byte)(1))) {
+                user.setVerified((byte)1);
             }
             super.addHash(user);
             if (savedList.get(user.getLoginid()) == null) {
