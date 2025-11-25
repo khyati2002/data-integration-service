7832e2bdb143a04f46684f61ca052f4dbea9a7d1
Kartik Rana <kartik.rana@salescode.ai>
2025-11-25 16:09:04 +0530

    Update GenericEntityService.java

--- src/main/java/com/applicate/services/channelkart/services/GenericEntityService.java
+++ src/main/java/com/applicate/services/channelkart/services/GenericEntityService.java
@@ -21,6 +21,9 @@ import static com.salescode.dim.jooq.generated.Tables.CK_GENERIC_OBJECT;
 public class GenericEntityService extends AbstractCDMService<GenericEntity> {
 	private static final Logger LOG = LoggerFactory.getLogger(GenericEntityService.class);
 	private GenericEntityRepository entityRepository;
+	public GenericEntityService() {
+		entityRepository = new GenericEntityRepository(getDslContext());
+	}
 
 	public List<List<GenericEntity>> getItemsToSaveList(List<GenericEntity> genericEntityList) {
 		List<List<GenericEntity>> result = new ArrayList<>();
