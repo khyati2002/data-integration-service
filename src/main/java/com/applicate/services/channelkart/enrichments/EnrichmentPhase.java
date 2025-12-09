2ea35d3dcd6979431f2002864479abe1587f7d38
khyati2002 <78091360+khyati2002@users.noreply.github.com>
2025-12-09 18:07:46 +0530

    Update EnrichmentPhase.java

--- src/main/java/com/applicate/services/channelkart/enrichments/EnrichmentPhase.java
+++ src/main/java/com/applicate/services/channelkart/enrichments/EnrichmentPhase.java
@@ -2,6 +2,6 @@ package com.applicate.services.channelkart.enrichments;
 
 public enum EnrichmentPhase {
 
-    PRE_VALIDATION, POST_VALIDATION, PRE_SAVE
+    PRE_VALIDATION, POST_VALIDATION, PRE_SAV
 
 }
